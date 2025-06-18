44f6cae65fe95ae4e2f1f2113b658be38d9ede15
Paul Haakma <paul_haakma@eagle.co.nz>
2025-06-18 20:28:34 +1200

    Add sample api responses to test folder

---
+++ tests/api_responses/users.py
@@ -0,0 +1,76 @@
+user = {
+    request: "https://data.linz.govt.nz/services/api/v1.x/users/me/viewers/428148/",
+    response: {
+        "id": 428148,
+        "url_html": null,
+        "url_canonical": null,
+        "source_viewer": null,
+        "title": "Map Viewer",
+        "data_extent": null,
+        "view_extent": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [175.4875373840332, -38.01658682647786],
+                        [175.4875373840332, -37.63992710164809],
+                        [176.10586166381836, -37.63992710164809],
+                        [176.10586166381836, -38.01658682647786],
+                        [175.4875373840332, -38.01658682647786],
+                    ]
+                ]
+            ],
+        },
+        "changed_at": "2025-06-07T04:50:16.980736Z",
+        "item_count": 0,
+        "basemap": "Streets",
+        "is_active": false,
+        "extras": '{"version": "v2", "dateAdded": "2025-06-03T01:47:59.121Z", "dateLastSelected": "2025-06-03T01:47:59.121Z", "inTab": true, "userEditedName": false, "publishedFromOtherMV": false, "paneSettings": {"layout": "map-pane-single", "panes": [{"is3d": false, "viewport": {"bearing": 0, "centre": [175.79671429627524, -37.828475045666636], "pitch": 0, "zoom": 11}, "isMain": true, "displaySettings": {"ThreeD": {"exaggeration": 1, "hillshadeDirection": 335, "hillshadeEnabled": true, "hillshadeExaggeration": 0.7, "terrainEnabled": true, "displayCompass": true, "renderUnderground3dLayers": true}, "TwoD": {"exaggeration": 1, "hillshadeDirection": 335, "hillshadeEnabled": false, "hillshadeExaggeration": 0.7, "terrainEnabled": false, "displayCompass": true, "tiltLock": 0, "rotationLock": 0, "renderUnderground3dLayers": true}}}]}}',
+        "pane_settings": {
+            "layout": "map-pane-single",
+            "panes": [
+                {
+                    "is3d": false,
+                    "viewport": {
+                        "bearing": 0,
+                        "centre": [175.79671429627524, -37.828475045666636],
+                        "pitch": 0,
+                        "zoom": 11,
+                    },
+                    "isMain": true,
+                    "displaySettings": {
+                        "ThreeD": {
+                            "exaggeration": 1,
+                            "hillshadeDirection": 335,
+                            "hillshadeEnabled": true,
+                            "hillshadeExaggeration": 0.7,
+                            "terrainEnabled": true,
+                            "displayCompass": true,
+                            "renderUnderground3dLayers": true,
+                        },
+                        "TwoD": {
+                            "exaggeration": 1,
+                            "hillshadeDirection": 335,
+                            "hillshadeEnabled": false,
+                            "hillshadeExaggeration": 0.7,
+                            "terrainEnabled": false,
+                            "displayCompass": true,
+                            "tiltLock": 0,
+                            "rotationLock": 0,
+                            "renderUnderground3dLayers": true,
+                        },
+                    },
+                }
+            ],
+        },
+        "geotags": [],
+        "generation_key": "09de7833b627e782bd6e376976a47ee8",
+        "source_client": "7aaa4d03-8877-44c9-8140-8d98e0e52afe",
+        "thumbnail_url": null,
+        "public_access": null,
+        "user_capabilities": [],
+        "user_permissions": ["find", "view", "edit", "admin"],
+        "items": [],
+        "num_views": 0,
+    },
+}
