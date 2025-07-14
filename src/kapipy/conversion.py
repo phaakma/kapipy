53cc4026932b9bf26132c86959e1ee4502639156
Paul Haakma <paul_haakma@eagle.co.nz>
2025-07-14 20:39:04 +1200

    Add CropFeaturesManager and CropLayersManager for managing crop features and layers; implement methods for fetching, listing, and accessing features and layers.

--- src/kapipy/conversion.py
+++ src/kapipy/conversion.py
@@ -368,6 +368,11 @@ def sdf_to_single_polygon_geojson(
     geo_json = geom.JSON  # this is Esri JSON
     return esri_json_to_geojson(geom.JSON, geom.geometry_type)
 
+def arcgis_polygon_to_geojson(geom):
+    geom = geom.project_as(4326)
+    geo_json = geom.JSON  # this is Esri JSON
+    return esri_json_to_geojson(geom.JSON, geom.geometry_type)
+
 def gdf_to_single_polygon_geojson(
     gdf: "gpd.GeoDataFrame",
 ) -> dict[str, Any] | None:
@@ -422,11 +427,14 @@ def get_data_type(obj: Any) -> str:
         try:
             import pandas as pd
             from arcgis.features import GeoAccessor
+            from arcgis.geometry import Polygon
 
             # SEDF is a pandas.DataFrame with a _spatial accessor
             # pandas.core.frame.DataFrame
             if isinstance(obj, pd.DataFrame) and hasattr(obj, "spatial"):
                 return "sdf"
+            if isinstance(obj, Polygon):
+                return "ARCGIS_POLYGON"
         except ImportError:
             pass
 
