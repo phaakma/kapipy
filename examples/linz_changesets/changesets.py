11e1714b420b4062ab5b11efbb9a45e03e6efaea
Paul Haakma <paul_haakma@eagle.co.nz>
2025-08-13 02:46:23 +1200

    Update with single target database. Change data_folder name to audit_folder and fgb to temp_fgb to better reflect the intention for those parameters.

--- examples/linz_changesets/changesets.py
+++ examples/linz_changesets/changesets.py
@@ -21,8 +21,8 @@ import yaml
 import keyring
 import arcpy
 
-def configure_logging(data_folder):
-    log_folder = os.path.join(data_folder, "logs")
+def configure_logging(audit_folder):
+    log_folder = os.path.join(audit_folder, "logs")
     os.makedirs(log_folder, exist_ok=True)
     log_file = os.path.join(log_folder, "linz.log")
 
@@ -67,34 +67,51 @@ def delete_fgb(target_fgb: str):
         logger.info(f"Deleting existing fgb: {target_fgb}")
         shutil.rmtree(target_fgb)
 
-def unzip_fgb(file_path: str, data_folder: str):
+def unzip_fgb(file_path: str, audit_folder: str):
     with zipfile.ZipFile(file_path, "r") as zip_ref:
         gdb_files = [f for f in zip_ref.namelist() if ".gdb/" in f]
         for file in gdb_files:
-            zip_ref.extract(file, data_folder)
+            zip_ref.extract(file, audit_folder)
 
-def process_exports(layers: dict, gisk, data_folder: str):
-    fgb_list = []    
+def copy_all_feature_classes(source_fgb, target_fgb):
+    if not arcpy.Exists(target_fgb):
+        t = Path(target_fgb)
+        out_folder_path = str(t.parent)
+        out_name = t.stem
+        t.parent.mkdir(parents=True, exist_ok=True)
+        arcpy.management.CreateFileGDB(out_folder_path, out_name)
+    arcpy.env.workspace = source_fgb
+    arcpy.env.overwriteOutput = True
+    for fc in arcpy.ListFeatureClasses():
+        target_fc = os.path.join(target_fgb, fc)
+        arcpy.conversion.ExportFeatures(fc, target_fc)
+        #editor tracking is optional but useful for future troubleshooting 
+        arcpy.management.EnableEditorTracking(
+            in_dataset=target_fc,
+            creator_field="created_user",
+            creation_date_field="created_date",
+            last_editor_field="last_edited_user",
+            last_edit_date_field="last_edited_date",
+            add_fields="ADD_FIELDS",
+            record_dates_in="UTC"
+        )
+        logger.info(f"Editor tracking enabled on: {target_fc}")
+
+def process_exports(layers: dict, gisk, audit_folder: str):
+ 
     for layer in layers:
-        crop_feature_url = None 
-        crop_feature_sdf = None                     
+        crop_feature_url = None                    
         crop_layer_id = layer.get("crop_layer_id")
         crop_feature_id = layer.get("crop_feature_id")
         if crop_layer_id is not None and crop_feature_id is not None:
             crop_feature_item = gisk.content.crop_layers.get(crop_layer_id).get(crop_feature_id)
-            crop_feature = crop_feature_item.get()
             crop_feature_url = crop_feature_item.url
-            crop_feature_sdf = crop_feature.sdf
 
         itm = gisk.content.get(layer.get("id"))
-        logger.info(f"Processing layer: {itm.id=}, {itm.title=}")
-        id_field = itm.data.primary_key_fields[0]
-        target_fgb = os.path.join(data_folder, layer.get("fgb"))
-        fgb_list.append(target_fgb)
-        target_fc = os.path.join(target_fgb, layer.get("featureclass"))
+        logger.info(f"Processing layer: {itm.id=}, {itm.title=}")        
         out_sr = layer.get("out_sr", 2193)
-
-        delete_fgb(target_fgb)
+        logger.info(layer.get("temp_fgb"))
+        delete_fgb(layer.get("temp_fgb"))
         #This initiates the export request from LINZ
         itm.export(export_format="geodatabase", out_sr=out_sr, extent=crop_feature_url)
 
@@ -102,31 +119,31 @@ def process_exports(layers: dict, gisk, data_folder: str):
     gisk.content.download(poll_interval=30)
 
     for job in gisk.content.jobs:
-        unzip_fgb(file_path=job.download_file_path, data_folder=data_folder)
+        unzip_fgb(file_path=job.download_file_path, audit_folder=audit_folder)
         os.remove(job.download_file_path)
 
-    for target_fgb in fgb_list:
-        #editor tracking is optional but useful for future troubleshooting
-        enable_editor_tracking(target_fgb)
+    # copy everything to final target database  
+    for layer in layers:
+        temp_fgb = os.path.join(audit_folder, layer.get("temp_fgb"))               
+        target_fgb = os.path.join(audit_folder, layer.get("target_db"))
+        copy_all_feature_classes(temp_fgb, target_fgb)        
 
 
-def process_changesets(layers: dict, gisk, data_folder: str):
+def process_changesets(layers: dict, gisk, audit_folder: str):
     for layer in layers:
         try:
-            crop_feature_url = None 
             crop_feature_sdf = None                     
             crop_layer_id = layer.get("crop_layer_id")
             crop_feature_id = layer.get("crop_feature_id")
             if crop_layer_id is not None and crop_feature_id is not None:
                 crop_feature_item = gisk.content.crop_layers.get(crop_layer_id).get(crop_feature_id)
                 crop_feature = crop_feature_item.get()
-                crop_feature_url = crop_feature_item.url
                 crop_feature_sdf = crop_feature.sdf
 
             itm = gisk.content.get(layer.get("id"))
             logger.info(f"Processing layer: {itm.id=}, {itm.title=}")
             id_field = itm.data.primary_key_fields[0]
-            target_fgb = os.path.join(data_folder, layer.get("fgb"))
+            target_fgb = os.path.join(audit_folder, layer.get("target_db"))
             target_fc = os.path.join(target_fgb, layer.get("featureclass"))
             out_sr = layer.get("out_sr", 2193)
 
@@ -138,20 +155,6 @@ def process_changesets(layers: dict, gisk, data_folder: str):
         except Exception as e:
             err = buildErrorMessage(e)
             logger.error(err, exc_info=True)
-
-def enable_editor_tracking(fgb):
-    arcpy.env.workspace = fgb
-    for fc in arcpy.ListFeatureClasses():        
-        arcpy.management.EnableEditorTracking(
-            in_dataset=fc,
-            creator_field="created_user",
-            creation_date_field="created_date",
-            last_editor_field="last_edited_user",
-            last_edit_date_field="last_edited_date",
-            add_fields="ADD_FIELDS",
-            record_dates_in="UTC"
-        )
-        logger.info(f"Editor tracking enabled on: {fc}")
 
 def get_changeset(itm, crop_sdf, out_sr, gisk):
     last_download_record = gisk.audit.get_latest_request_for_item(
@@ -187,7 +190,7 @@ def configure_layers(config):
         layers.append(layer)
     return layers
 
-def main(args):
+def main(args):    
     
     script_dir = Path(__file__).parent.resolve()
     config_path = script_dir / args.file
@@ -195,22 +198,22 @@ def main(args):
     with open(config_path) as f:
         config = yaml.safe_load(f)
     
-    data_folder = config["data_folder"]
+    audit_folder = config["audit_folder"]
     global logger # global so we can use it all through the script
-    logger = configure_logging(data_folder)
+    logger = configure_logging(audit_folder)
     layers = configure_layers(config) 
 
     # Connect to LINZ
     # assumes api key previously stored using keyring
     linz_api_key = keyring.get_password("kapipy", "linz")
     linz = GISK(name="linz", api_key=linz_api_key)
-    linz.audit.enable_auditing(folder=data_folder)
-    linz.content.download_folder = data_folder
+    linz.audit.enable_auditing(folder=audit_folder)
+    linz.content.download_folder = audit_folder
 
     if args.export:
-        process_exports(layers=layers, gisk=linz, data_folder=data_folder)
+        process_exports(layers=layers, gisk=linz, audit_folder=audit_folder)
     elif args.changeset:
-        process_changesets(layers=layers, gisk=linz, data_folder=data_folder)
+        process_changesets(layers=layers, gisk=linz, audit_folder=audit_folder)
     
     logger.info(f"Finished processing all layers")
 
